a15ac71d0ceb92d94b27fea5c09b0ec883e9e17c
Shawn Tennity <shawntennity@gmail.com>
2020-03-24 17:47:37 -0400

    Set up socket.io

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const socketio = require('socket.io')
 
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when a client connects
+io.on('connection', socket => {
+  console.log('New WS Connection...');
+
+  socket.emit('message', 'Welcome to Chatroom!')
+})
 
 const PORT = 3000 || process.env.PORT;
 
