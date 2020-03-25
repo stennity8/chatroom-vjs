7694343cd7a04c0ce871fd66a750b4c3675871e4
Shawn Tennity <shawntennity@gmail.com>
2020-03-25 17:47:21 -0400

    Set up message formatting.

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,13 @@
 const chatForm = document.getElementById('chat-form')
+const chatMessages = document.querySelector('.chat-messages')
 const socket = io();
 
 // Message from server
 socket.on('message', message => {
-  console.log(message);
   outputMessage(message);
+
+  // Scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight
 });
 
 // Message submti
@@ -16,15 +19,19 @@ chatForm.addEventListener('submit', (e) => {
 
   // Emit message to server
   socket.emit('chatMessage', msg)
+
+  // Clear input
+  e.target.elements.msg.value = ''
+  e.target.elements.msg.focus()
 });
 
 // Output message to DOM
-function outputMessage(message) {
+function outputMessage({ username, text, time }) {
   const div = document.createElement('div');
   div.classList.add('message');
-  div.innerHTML = `<p className="meta">Brad <span>9:12pm</span></p>
+  div.innerHTML = `<p className="meta">${username} <span>${time}</span></p>
   <p className="text">
-    ${message}
+    ${text}
   </p>`
   document.querySelector('.chat-messages').appendChild(div);
 }
